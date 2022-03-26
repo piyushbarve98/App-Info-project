e4d986ededd2fe3a7308fe62e6ac7d762dc3ac1c
piyushbarve98 <barvepiyush1000@gmail.com>
2022-03-26 19:08:54 +0000

    css added

--- main.js
+++ main.js
@@ -13,16 +13,51 @@ onSnapshot(q,(snapshot)=>{
         const image = document.createElement('img');
         const appName = document.createElement('span');
         const desc = document.createElement('span');
+        const button = document.createElement('button');
 
+        const textDiv = document.createElement('div');
+
+        textDiv.className = "textDiv";
         card.append(image);
-        card.append(appName);
-        card.append(desc);
+        card.append(textDiv);
+        textDiv.append(appName);
+        textDiv.append(desc);
+       
         
         App_list.append(card);
+
+        appName.className = "app-title";
+        desc.className = "description";
 
         image.src = doc.data().imageUrl;
         appName.textContent = doc.data().Name;
         desc.textContent = doc.data().Description;
+        
+        const buttonDiv = document.createElement('div');
+        buttonDiv.className = 'buttonDiv';
+        
+        card.append(buttonDiv);
+        buttonDiv.append(button);
+        button.textContent = 'Install';
+        button.className = 'installButton';
+        
+
+        const modal = document.querySelector(".modal");
+        const modalBox = document.querySelector(".box");
+
+        
+        
+        button.onclick = function(){
+            modal.classList.add("is-active");
+            const nextButton = document.createElement("button");
+            nextButton.textContent = "Next";
+            modalBox.innerHTML = "";
+            modalBox.append(nextButton);
+        }
+        const closeModalButton = document.querySelector('.modal-close');
+        closeModalButton.onclick = function(){
+            modal.classList.remove("is-active");
+        }
     });
     
 });
